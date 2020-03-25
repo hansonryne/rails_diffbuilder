7cf6730cee3be81b41b0042d8a3724d23204054e
Ryne Hanson <hansonryne@gmail.com>
2020-03-25 10:04:26 -0400

    Add Git, validate Repos

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,7 @@
 class Repository < ApplicationRecord
   has_many :reviews, :dependent => :destroy
+
+  validates :name, :presence => true
+  validates :project, :presence => true
+  validates :repo_location, :presence => true
 end
