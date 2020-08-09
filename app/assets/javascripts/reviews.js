cd247c81a468d90330891724c0569d74bddc700a
Ryne Hanson <hansonryne@gmail.com>
2020-08-09 18:09:47 -0400

    Almost done refactoring UI

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,11 +1,13 @@
-function toggle_file_vis(name) {
-        var y = name;
+function toggle_file_vis(tgt) {
+        var y = tgt.name;
         console.log(y);
-        var x = document.getElementsByClassName("file-"+name);
+        var x = document.getElementsByClassName("file-" + tgt.name);
         for (var i = 0; i < x.length; i++) {
                 if (x[i].style.display === "none") {
+                        tgt.style.backgroundColor = "#2C7A7B";
                         x[i].style.display = "";
                 } else {
+                        tgt.style.backgroundColor = "#EDF2F7";
                         x[i].style.display = "none";
                 }
         }
