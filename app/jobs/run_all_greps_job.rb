03adac2511bfa8881bba19af3a192018954725f3
Ryne Hanson <hansonryne@gmail.com>
2020-08-13 15:28:48 -0400

    Run only new greps on a repo

--- app/jobs/run_all_greps_job.rb
+++ app/jobs/run_all_greps_job.rb
@@ -1,27 +1,29 @@
 class RunAllGrepsJob < ApplicationJob
   include ActiveJob::Status
   queue_as :default
-
+  
   def perform(repository)
     puts self.job_id
     repo_path = Rails.root.join('storage', 'repositories', repository.secret_path)
     files = repository.get_files_for_grep(repo_path)
     progress.total = files.count
-
-
+    
+    
     results = []
-
-    files.each do |file|
-      File.foreach(file) do |line|
-        repository.greps.each do |grep|
-          if line.include?(grep.search_value)
-            results << { grep_id: grep.id, filename: "#{file.sub(repo_path.to_s + '/', '')}", line_number: $. }
+    
+    repository.greps.each do |grep|
+      if grep.results == "Run me"
+        files.each do |file|
+          File.foreach(file) do |line|
+            if line.include?(grep.search_value)
+              results << { grep_id: grep.id, filename: "#{file.sub(repo_path.to_s + '/', '')}", line_number: $. }
+            end
           end
         end
       end
       progress.increment
     end
-
+    
     repository.greps.each do |g|
       local_results = results.select {|result| result[:grep_id] == g.id}
       if local_results.present?
